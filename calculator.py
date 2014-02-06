c89ef5873c2404a9b31bedaad57699b4f36610a0
Hackbright Student <user@mini-003.(none)>
2014-02-05 17:41:41 -0800

    our calculator works and can handle value errors

--- calculator.py
+++ calculator.py
@@ -7,41 +7,47 @@ while True:
     userinput = raw_input()
     
     tokens = userinput.split(" ")
+    
+    if tokens[0] == "+" or tokens[0] == "-" or tokens[0] == "/" or tokens[0] == "*" or tokens[0] == "pow" or tokens[0] == "mod":
+        try:
+            int(tokens[1])
+            int(tokens[2])
+        except ValueError: 
+            print "use your numbers"
+            continue
+    elif tokens[0] == "square" or tokens[0] == "cube":
+        try:
+            int(tokens[1])
+        except ValueError:
+            print "use your numbers"
+            continue
 
     if tokens[0] == "q":
         break
     
-    if tokens[0] == "+":
+    elif tokens[0] == "+":
         print arithmetic.add(int(tokens[1]),int(tokens[2]))
-        continue
 
-    if tokens[0] == "-":
+    elif tokens[0] == "-":
         print arithmetic.subtract(int(tokens[1]),int(tokens[2]))
-        continue
-
-    if tokens[0] == "*":
+    
+    elif tokens[0] == "*":
         print arithmetic.multiply(int(tokens[1]),int(tokens[2]))
-        continue
 
-    if tokens[0] == "/":
+    elif tokens[0] == "/":
         print arithmetic.divide(float(tokens[1]),float(tokens[2]))
-        continue
 
-    if tokens[0] == "square":
+    elif tokens[0] == "square":
         print arithmetic.square(int(tokens[1]))
-        continue
 
-    if tokens[0] == "cube":
+    elif tokens[0] == "cube":
         print arithmetic.cube(int(tokens[1]))
-        continue
 
-    if tokens[0] == "pow":
+    elif tokens[0] == "pow":
         print arithmetic.power(int(tokens[1]),int(tokens[2]))
-        continue
 
-    if tokens[0] == "mod":
+    elif tokens[0] == "mod":
         print arithmetic.mod(int(tokens[1]),int(tokens[2]))
-        continue
 
     else:
          print "Sorry, I can't process that"
