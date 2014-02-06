637f2abf405a62f6d36b4b9d98e2559becb45c87
Hackbright Student <user@mini-003.(none)>
2014-02-05 18:04:03 -0800

    Added error functionality for too many values but it's not working quite yet

--- calculator.py
+++ calculator.py
@@ -7,20 +7,42 @@ while True:
     userinput = raw_input()
     
     tokens = userinput.split(" ")
+
+    doubleoperators = ["+","-","*","/","mod","pow"]
+    singleoperators = ["cube","square"]
     
-    if tokens[0] == "+" or tokens[0] == "-" or tokens[0] == "/" or tokens[0] == "*" or tokens[0] == "pow" or tokens[0] == "mod":
+    #Checking that the values are numbers in two value expresions
+    if tokens[0] in doubleoperators:
         try:
             int(tokens[1])
             int(tokens[2])
         except ValueError: 
             print "use your numbers"
             continue
-    elif tokens[0] == "square" or tokens[0] == "cube":
+
+    #Checking that the value is a number in one value expressions
+    elif tokens[0] in singleoperators:
         try:
             int(tokens[1])
         except ValueError:
             print "use your numbers"
             continue
+
+    #Dealing with extra values for two value expressions
+    if tokens[0] in doubleoperators:
+        try:
+           tokens[3] == True
+        except IndexError: 
+           print "I can only handle two values with that expression"
+           continue
+
+    #Dealing with extra values for one value expressions
+    elif tokens[0] == singleoperators:
+        try:
+            tokens[2] == True
+        except IndexError:
+          print "I can only handle one value with this expression"
+        
 
     if tokens[0] == "q":
         break
