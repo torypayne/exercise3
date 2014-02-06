89741d6762ae68537a5fa41eb80dc207854514ec
Hackbright Student <user@mini-003.(none)>
2014-02-05 17:07:05 -0800

    Working basic calculator, can still add some exceptions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,47 @@
-print "Calculator here, at your service! Give me numbers to math or q to get out of here"
-userinput = raw_input()
+import arithmetic
 
-tokens = userinput.split(" ")
-print tokens
+print "Calculator here, at your service! Give me numbers to math or q to get out of here"
+
+while True:
+    
+    userinput = raw_input()
+    
+    tokens = userinput.split(" ")
+
+    if tokens[0] == "q":
+        break
+    
+    if tokens[0] == "+":
+        print arithmetic.add(int(tokens[1]),int(tokens[2]))
+        continue
+
+    if tokens[0] == "-":
+        print arithmetic.subtract(int(tokens[1]),int(tokens[2]))
+        continue
+
+    if tokens[0] == "*":
+        print arithmetic.multiply(int(tokens[1]),int(tokens[2]))
+        continue
+
+    if tokens[0] == "/":
+        print arithmetic.divide(float(tokens[1]),float(tokens[2]))
+        continue
+
+    if tokens[0] == "square":
+        print arithmetic.square(int(tokens[1]))
+        continue
+
+    if tokens[0] == "cube":
+        print arithmetic.cube(int(tokens[1]))
+        continue
+
+    if tokens[0] == "pow":
+        print arithmetic.power(int(tokens[1]),int(tokens[2]))
+        continue
+
+    if tokens[0] == "mod":
+        print arithmetic.mod(int(tokens[1]),int(tokens[2]))
+        continue
+
+    else:
+         print "Sorry, I can't process that"
